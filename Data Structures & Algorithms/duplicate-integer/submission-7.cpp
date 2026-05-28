b134b3395b8c0b61d796fa06affe858e0c056b0d
gum-beer <73459346+gum-beer@users.noreply.github.com>
2026-05-28 11:08:19 +0530

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> s1;
+        for(int i=0;i<nums.size();i++)
+        {
+            s1.insert(nums[i]);
+        }
+        if(s1.size()==nums.size())
+        {
+            return false;
+        }
+        return true;
+    }
+};
