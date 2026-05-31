c083752216ab1d95d5a82923d39b2cf9e40936d7
gum-beer <73459346+gum-beer@users.noreply.github.com>
2026-06-01 02:00:29 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> res;
+        for(int i=0;i<nums.size();i++)
+        {
+            int prod=1;
+            for(int j=0;j<nums.size();j++)
+            {
+                if(i!=j)
+                {
+                    prod *=nums[j];
+                }
+            }
+            res.push_back(prod);
+        }
+        return res;
+    }
+};
