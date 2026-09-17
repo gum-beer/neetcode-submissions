deca9316a7c442ce4ff46caf06a58c5f9f5518b3
gum-beer <73459346+gum-beer@users.noreply.github.com>
2026-09-17 09:02:13 +0530

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1,ListNode* l2)
+    {
+        ListNode dummy;
+        ListNode* curr=&dummy;
+        while(l1 and l2)
+        {
+            if(l1->val>l2->val)
+            {
+                curr->next=l2;
+                l2=l2->next;
+                curr=curr->next;
+            }
+            else{
+                curr->next=l1;
+                l1=l1->next;
+                curr=curr->next;
+            }
+        }
+        if(l1)
+        {
+            curr->next=l1;
+        }
+        if(l2){
+            curr->next=l2;
+        }
+        return dummy.next;
+        
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0)
+        {
+            return{};
+        }
+        while(lists.size()>1)
+        {
+            vector<ListNode*> res;
+            for(int i=0;i<lists.size();i+=2)
+            {
+                ListNode* temp1=lists[i];
+                ListNode* temp2= (i+1)<lists.size()? lists[i+1]:nullptr;
+                res.push_back(mergeTwoLists(temp1,temp2));
+            }
+            lists=res;
+        }
+        return lists[0];
+        
+        
+    }
+};
